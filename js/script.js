bfafcff2dab1956e4fa62f0a3d0730ad9098e0a5
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-01-24 00:04:34 +0100

    add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -37,14 +37,19 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE[ get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute("href");
     console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attrubute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attrubute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
 
 }
 
