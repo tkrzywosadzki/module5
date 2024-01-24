389057ac08853783580cc95f881ab1cb260f2bfc
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-01-24 01:09:54 +0100

    add generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -57,3 +57,60 @@ for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }
 
+
+/* generate title links script */
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+    /*remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+
+    titleList.innerHTML = '';
+
+    /*for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+    //console.log(articles);
+
+    let html = '';
+        /* get the article id */
+
+    for(let article of articles){
+        const articleId = article.getAttribute("id");
+        // console.log(articleId);
+
+        /* find the title element */
+
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        // console.log(articleTitle);
+
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        // console.log(linkHTML);
+
+        /* insert link into titleList */
+
+        // titleList.innerHTML = titleList.innerHTML + linkHTML;
+        html = html + linkHTML;
+        console.log(html);
+
+    }
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+
+    for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+    }
+
+}
+
+generateTitleLinks();
